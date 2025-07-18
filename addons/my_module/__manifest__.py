6336a56ce7a0cb67d7500fad2fde9ccfbd0ca1bc
MaskedMir <maratbeibutov@ya.ru>
2025-07-18 17:19:37 +0300

    First commit

---
+++ addons/my_module/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "My Product Thing",
+    "version": "1.0",
+    "depends": ["base"],
+    "author": "Your Name",
+    "data": [
+        "security/ir.model.xml",
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "views/product_thing_views.xml"
+    ],
+    "installable": True
+}
